56383d924e49af796d050aba8ba6cf20b217347e
Scott C. Reynolds <scott@scottcreynolds.com>
2013-10-24 13:04:59 -0400

    more model specs

--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -1,8 +1,10 @@
 require_relative '../spec_helper.rb'
 
 describe "House" do
+  it "has an address"
   it "has candy"
   it "can stock candy"
   it "can be trick-or-treated at"
   it "knows if a kid is too old to trick-or-treat"
+  it "knows what kids will visit it"
 end
--- spec/models/kid_spec.rb
+++ spec/models/kid_spec.rb
@@ -3,6 +3,7 @@ require_relative '../spec_helper.rb'
 describe "Kid" do
   it "has a bucket"
   it "has a collection route"
+  it "has a list of houses on the collection route"
   it "has a name"
   it "has an age"
   describe ".trick_or_treat" do
