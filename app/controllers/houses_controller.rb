c7fdead8b0fc74d5f86d7bbdbb9d4f3b8070ede6
Katie Hoffman <ktahoffman@gmail.com>
2014-07-01 18:03:46 -0400

    add routes for houese plus specs for house index and house show

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,2 +1,11 @@
 class HousesController < HalloweenController
+  get '/houses' do
+    @houses = House.all
+    erb :'houses/index'
+  end
+
+  get '/houses/:id' do
+    @house = House.find_by(:id => params[:id])
+    erb :'houses/show'
+  end
 end
--- spec/controllers/houses_controller_spec.rb
+++ spec/controllers/houses_controller_spec.rb
@@ -1,24 +1,45 @@
 require 'spec_helper'
 
 describe "HousesController" do
-  it "allows you to list all houses" do
-    pending
+  describe "GET /houses" do
+    before do
+      House.create(address: "123 S. Seaside Ln.")
+      House.create(address: "1019 Expensive Ave.")
+      get '/houses'
+    end
+    it "responds with a 200 status code" do
+      expect(last_response).to be_ok
+    end
+    it "renders the houses index template" do
+      expect(last_response.body).to include("<h1>All Houses</h1>")
+      expect(last_response.body).to include("123 S. Seaside Ln.")
+      expect(last_response.body).to include("1019 Expensive Ave.")
+    end
   end
 
-  it "allows you to see a house" do
-    pending
+  describe "GET /houses/:id" do
+    before do
+      cottage = House.create(address: "123 S. Seaside Ln.")
+      get "/houses/#{cottage.id}"
+    end
+    it "allows you to see a house" do
+      expect(last_response).to be_ok
+    end
+    it "renders the house show template" do
+      expect(last_response.body).to include("123 S. Seaside Ln.")
+    end
   end
 
-  it "allows you to trick-or-treat the house" do
+  xit "allows you to trick-or-treat the house" do
     pending
   end
 
   # BONUS: Implement the forms required to do the following:
-  it "allows you to add a house" do
+  xit "allows you to add a house" do
     pending
   end
 
-  it "allows you to edit a house" do
+  xit "allows you to edit a house" do
     pending
   end
 end
