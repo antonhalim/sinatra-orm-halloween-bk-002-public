67a94ce5428e3e828c97edafd39310cd81ee6406
Katie Hoffman <ktahoffman@gmail.com>
2014-07-01 16:48:16 -0400

    adds house specs

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,3 +1,12 @@
 class House < ActiveRecord::Base
   has_many :candies
+
+  def give_candy(kid)
+    if kid.age < 17
+      sweet = self.candies[0]
+      kid.bucket.candies << sweet
+    else
+      "You're too old to trick-or-treat."
+    end
+  end
 end
--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -1,19 +1,32 @@
 require 'spec_helper'
 
 describe "House" do
+  before do
+    @cottage = House.create(:address=>"132 Seabridge Ln")
+    @skittles = Candy.create(:name => "Skittles", :size => 3, :pieces => 20)
+    @milkyway = Candy.create(:name => "Milkyway", :size => 2, :pieces => 2)
+    @redhots = Candy.create(:name => "Redhots", :size => 2, :pieces => 10)
+    @kid = Kid.create(:name => "Arel", :age => 14)
+    @kid.bucket = Bucket.new
+    @cottage.candies << [@skittles, @milkyway, @redhots]
+  end
   it "has an address" do
-    pending
+    expect(@cottage.address).to eq("132 Seabridge Ln")
   end
 
   it "can be trick-or-treated at" do
-    # Should give a piece of candy
-    
-    pending
+    expect(@kid.bucket.candies.count).to eq(0)
+    expect(@cottage.candies[0].bucket_id).to eq(nil)
+    @cottage.give_candy(@kid)
+    expect(@kid.bucket.candies.count).to eq(1)
+    expect(@cottage.candies[0].bucket_id).to eq(@kid.bucket.id)
   end
 
   it "knows if a kid is too old to trick-or-treat" do
-    # Should not give candy if kid above 17
-
-    pending
+    avi = Kid.create(:name => "Avi", :age => 30)
+    avi.bucket = Bucket.new
+    expect(@cottage.give_candy(avi)).to eq("You're too old to trick-or-treat.")
+    expect(avi.bucket.candies.count).to eq(0)
+    expect(@cottage.candies[0].bucket_id).to eq(nil)
   end
 end
