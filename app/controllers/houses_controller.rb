f751e4bf4424691473c34e220327e09fd53d6b2a
Katie Hoffman <ktahoffman@gmail.com>
2014-07-01 18:10:50 -0400

    add route for trick-or-treating

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -8,4 +8,12 @@ class HousesController < HalloweenController
     @house = House.find_by(:id => params[:id])
     erb :'houses/show'
   end
+
+  post '/houses/:id/trick-or-treat' do
+    @house = House.find_by(:id => params[:id])
+    @kid = Kid.find_by(:id => params[:kid_id])
+    @house.give_candy(@kid)
+    
+    erb :'houses/show'
+  end
 end
