e8ef2ee7f5ab9d0cdb5661cd44a149e4126f820e
Katie Hoffman <ktahoffman@gmail.com>
2014-07-01 18:19:49 -0400

    makes spec more complete

--- spec/controllers/houses_controller_spec.rb
+++ spec/controllers/houses_controller_spec.rb
@@ -3,30 +3,46 @@ require 'spec_helper'
 describe "HousesController" do
   describe "GET /houses" do
     before do
-      House.create(address: "123 S. Seaside Ln.")
-      House.create(address: "1019 Expensive Ave.")
+      @cottage = House.create(address: "123 S. Seaside Ln.")
+      @mansion = House.create(address: "1019 Expensive Ave.")
+      @skittles = Candy.create(:name => "Skittles", :size => 3, :pieces => 20)
+      @milkyway = Candy.create(:name => "Milkyway", :size => 2, :pieces => 2)
+      @redhots = Candy.create(:name => "Redhots", :size => 2, :pieces => 10)
+      @twix = Candy.create(:name => "Twix", :size => 2, :pieces => 2)
+      @mints = Candy.create(:name => "Junior Mints", :size => 4, :pieces => 20)
+      @sourpatch = Candy.create(:name => "Sour Patch Kids", :size => 3, :pieces => 15)
+      @cottage.candies << [@redhots, @twix, @mints, @sourpatch] 
+      @mansion.candies << [@skittles, @milkyway]
       get '/houses'
     end
     it "responds with a 200 status code" do
       expect(last_response).to be_ok
     end
-    it "renders the houses index template" do
+    it "renders the houses index template with candies" do
       expect(last_response.body).to include("<h1>All Houses</h1>")
       expect(last_response.body).to include("123 S. Seaside Ln.")
       expect(last_response.body).to include("1019 Expensive Ave.")
+      expect(last_response.body).to include(@twix.name)
     end
   end
 
   describe "GET /houses/:id" do
     before do
-      cottage = House.create(address: "123 S. Seaside Ln.")
-      get "/houses/#{cottage.id}"
+      @cottage = House.create(address: "123 S. Seaside Ln.")
+      @redhots = Candy.create(:name => "Redhots", :size => 2, :pieces => 10)
+      @twix = Candy.create(:name => "Twix", :size => 2, :pieces => 2)
+      @mints = Candy.create(:name => "Junior Mints", :size => 4, :pieces => 20)
+      @sourpatch = Candy.create(:name => "Sour Patch Kids", :size => 3, :pieces => 15)
+      @cottage.candies << [@redhots, @twix, @mints, @sourpatch] 
+      get "/houses/#{@cottage.id}"
     end
     it "allows you to see a house" do
       expect(last_response).to be_ok
     end
     it "renders the house show template" do
       expect(last_response.body).to include("123 S. Seaside Ln.")
+      expect(last_response.body).to include(@sourpatch.name)
+      expect(last_response.body).to include(@redhots.name)
     end
   end
 
