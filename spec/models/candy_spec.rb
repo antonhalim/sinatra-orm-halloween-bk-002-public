fd49b6a5b83bc9713b61cee609f6ff8e08fb0875
Katie Hoffman <ktahoffman@gmail.com>
2014-07-01 16:51:10 -0400

    adds candy spec

--- spec/models/candy_spec.rb
+++ spec/models/candy_spec.rb
@@ -1,15 +1,16 @@
 require 'spec_helper'
 
 describe "Candy" do
+  let(:skittles) { Candy.create(:name => "Skittles", :size => 3, :pieces => 20) }
   it "has a name" do
-    pending
+    expect(skittles.name).to eq("Skittles")
   end
 
   it "has a number of pieces per package" do
-    pending
+    expect(skittles.pieces).to eq(20)
   end
 
   it "has a size" do
-    pending
+    expect(skittles.size).to eq(3)
   end
 end
