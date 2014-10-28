d82b4f5a60244a7fa90e8778e2abcdb14613c954
Katie Hoffman <ktahoffman@gmail.com>
2014-10-28 12:14:53 -0400

    remove houses controller

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,47 +1,5 @@
 class HousesController < HalloweenController
-
-  get "/houses" do
-    @houses = House.all
-    erb :'houses/index'
-  end
-
-  get '/houses/new' do
-    erb :'houses/new'
-  end
-
-  get '/houses/:id/edit' do
-    @house = House.find(params[:id])
-    erb :'houses/edit'
-  end
-
-  get '/houses/:id' do
-    @house = House.find(params[:id])
-    erb :'houses/show'
-  end
-
-  post '/houses/:id/trick-or-treat' do
-    @house = House.find(params[:id])
-    @kid = Kid.find(params[:kid_id])
-    @house.give_candy(@kid)
-    redirect "/houses/#{@house.id}"
-  end
-
-  post '/houses/:id' do
-    @house = House.find(params[:id])
-    @house.update(params["house"])
-    redirect "/houses/#{@house.id}"
-  end
-
-  post "/houses" do
-    @house = House.create(params["house"])
-    if params["candy"]
-      params["candy"].each do |number, details|
-        c = Candy.new(details)
-        c.house_id = @house.id
-        c.save
-      end
-    end
-    redirect "/houses/#{@house.id}"
-  end
+  
+  # index, show, edit, update, new, create, trick-or-treat
 
 end
