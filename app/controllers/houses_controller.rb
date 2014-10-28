93d602c9361bdf6d418c176c8380e1d32bc4bdce
Katie Hoffman <ktahoffman@gmail.com>
2014-10-28 12:27:17 -0400

    add tests passing

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,5 +1,47 @@
 class HousesController < HalloweenController
-  
-  # index, show, edit, update, new, create, trick-or-treat
+
+  get "/houses" do
+    @houses = House.all
+    erb :'houses/index'
+  end
+
+  get '/houses/new' do
+    erb :'houses/new'
+  end
+
+  get '/houses/:id/edit' do
+    @house = House.find(params[:id])
+    erb :'houses/edit'
+  end
+
+  get '/houses/:id' do
+    @house = House.find(params[:id])
+    erb :'houses/show'
+  end
+
+  post '/houses/:id/trick-or-treat' do
+    @house = House.find(params[:id])
+    @kid = Kid.find(params[:kid_id])
+    @house.give_candy(@kid)
+    redirect "/houses/#{@house.id}"
+  end
+
+  post '/houses/:id' do
+    @house = House.find(params[:id])
+    @house.update(params["house"])
+    redirect "/houses/#{@house.id}"
+  end
+
+  post "/houses" do
+    @house = House.create(params["house"])
+    if params["candy"]
+      params["candy"].each do |number, details|
+        c = Candy.new(details)
+        c.house_id = @house.id
+        c.save
+      end
+    end
+    redirect "/houses/#{@house.id}"
+  end
 
 end
